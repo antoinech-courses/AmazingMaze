ecb5f8bd7fadd294ac53e3d621fbd5430ab82147
NotArobase <maximegrenet@gmail.com>
2024-12-17 10:01:16 +0100

    version 2.1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-// file: maze.rs
-
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 // Enumeration to represent the exploration status
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Exploration {
-    Explored,           // Fully explored
-    PartiallyExplored,  // Left child explored, right child to be explored
-    UnExplored,         // Not yet explored
+    Explored,
+    PartiallyExplored,
+    UnExplored,
 }
 
 // Enumeration to represent the maze
@@ -16,9 +14,9 @@ enum Exploration {
 enum Maze {
     Branch {
         label: String,
-        left: Rc<Maze>,
-        right: Rc<Maze>,
-        status: RefCell<Exploration>, // Enables interior mutability for exploration status
+        left: Arc<Maze>,
+        right: Arc<Maze>,
+        status: Mutex<Exploration>, // Interior mutability with Mutex
     },
     Leaf {
         label: String,
@@ -27,49 +25,45 @@ enum Maze {
 
 impl Maze {
     // Helper function to create a leaf
-    fn leaf(label: &str) -> Rc<Maze> {
-        Rc::new(Maze::Leaf {
+    fn leaf(label: &str) -> Arc<Maze> {
+        Arc::new(Maze::Leaf {
             label: label.to_string(),
         })
     }
 
     // Helper function to create a branch
-    fn branch(label: &str, left: Rc<Maze>, right: Rc<Maze>) -> Rc<Maze> {
-        Rc::new(Maze::Branch {
+    fn branch(label: &str, left: Arc<Maze>, right: Arc<Maze>) -> Arc<Maze> {
+        Arc::new(Maze::Branch {
             label: label.to_string(),
             left,
             right,
-            status: RefCell::new(Exploration::UnExplored), // Initial exploration status
+            status: Mutex::new(Exploration::UnExplored),
         })
     }
 
-    // Concurrent version of the exploration function
-    fn explore(&self, node: Rc<Maze>, work: &mut Vec<Rc<Maze>>, trace: &mut Vec<String>) {
+    // Concurrent exploration function
+    fn explore(&self, work: &Arc<Mutex<Vec<Arc<Maze>>>>, trace: &mut Vec<String>) {
         match self {
             Maze::Branch { label, left, right, status } => {
-                let current_status = *status.borrow();
-                match current_status {
+                let mut current_status = status.lock().unwrap();
+                match *current_status {
                     Exploration::UnExplored => {
-                        // Push the current branch onto the work stack
-                        status.replace(Exploration::PartiallyExplored); // Update the status
+                        *current_status = Exploration::PartiallyExplored;
                         trace.push(label.clone());
-                        work.push(node.clone()); // Push the current node to the work stack
-                        left.explore(Rc::clone(left), work, trace); // Explore the left child
+                        work.lock().unwrap().push(Arc::clone(left));
+                        work.lock().unwrap().push(Arc::clone(self));
                     }
                     Exploration::PartiallyExplored => {
-                        // The left child has already been explored, now explore the right
-                        status.replace(Exploration::Explored); // Mark this branch as fully explored
+                        *current_status = Exploration::Explored;
                         trace.push(label.clone());
-                        right.explore(Rc::clone(right), work, trace); // Explore the right child
+                        work.lock().unwrap().push(Arc::clone(right));
                     }
                     Exploration::Explored => {
-                        // If the branch is already fully explored, just add it to the trace
                         trace.push(label.clone());
                     }
                 }
             }
             Maze::Leaf { label } => {
-                // Leaves are always added to the trace
                 trace.push(label.clone());
             }
         }
@@ -77,19 +71,17 @@ impl Maze {
 }
 
 // Function to construct the maze
-fn maze() -> Rc<Maze> {
-    // Create the leaves
+fn maze() -> Arc<Maze> {
     let leaf2 = Maze::leaf("2");
     let leaf4 = Maze::leaf("4");
     let leaf5 = Maze::leaf("5");
     let leaf8 = Maze::leaf("8");
 
-    // Create the branches
-    let branch3 = Maze::branch("3", Rc::clone(&leaf4), Rc::clone(&leaf5));
-    let branch1 = Maze::branch("1", Rc::clone(&leaf2), Rc::clone(&branch3));
-    let branch7 = Maze::branch("7", Rc::clone(&leaf5), Rc::clone(&leaf8));
-    let branch6 = Maze::branch("6", Rc::clone(&branch3), Rc::clone(&branch7));
-    let branch0 = Maze::branch("0", Rc::clone(&branch1), Rc::clone(&branch6));
+    let branch3 = Maze::branch("3", Arc::clone(&leaf4), Arc::clone(&leaf5));
+    let branch1 = Maze::branch("1", Arc::clone(&leaf2), Arc::clone(&branch3));
+    let branch7 = Maze::branch("7", Arc::clone(&leaf5), Arc::clone(&leaf8));
+    let branch6 = Maze::branch("6", Arc::clone(&branch3), Arc::clone(&branch7));
+    let branch0 = Maze::branch("0", Arc::clone(&branch1), Arc::clone(&branch6));
 
     branch0
 }
@@ -98,14 +90,35 @@ fn main() {
     // Build the maze
     let maze = maze();
 
-    // Initialize the work stack with the root node
-    let mut work: Vec<Rc<Maze>> = vec![Rc::clone(&maze)];
-    let mut trace: Vec<String> = vec![];
+    // Shared work stack and trace
+    let work = Arc::new(Mutex::new(vec![Arc::clone(&maze)]));
+    let trace = Arc::new(Mutex::new(Vec::new()));
 
-    // Simulate concurrent exploration
-    while !work.is_empty() {
-        let node = work.pop().expect("work stack should not be empty");
-        node.explore(Rc::clone(&node), &mut work, &mut trace);
-        println!("Trace so far: {:?}", trace);
+    // Number of threads
+    let num_threads = 4;
+    let mut handles = vec![];
+
+    for i in 0..num_threads {
+        let work_clone = Arc::clone(&work);
+        let trace_clone = Arc::clone(&trace);
+
+        // Spawn a thread
+        let handle = thread::spawn(move || {
+            while let Some(node) = work_clone.lock().unwrap().pop() {
+                node.explore(&work_clone, &mut trace_clone.lock().unwrap());
+                println!("Thread {} processed a node", i);
+                thread::yield_now(); // Allow other threads to run
+            }
+        });
+        handles.push(handle);
     }
+
+    // Wait for all threads to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // Print the final trace
+    let trace_result = trace.lock().unwrap();
+    println!("Final Trace: {:?}", *trace_result);
 }
