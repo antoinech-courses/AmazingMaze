f3dc4d2eae7135f0ca80e9259796ed76427bf4d8
NotArobase <maximegrenet@gmail.com>
2024-12-17 10:38:48 +0100

    sleep + new version

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::thread;
 // Enumeration to represent the exploration status
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Exploration {
-    Explored,
-    PartiallyExplored,
-    UnExplored,
+    Explored,           // Fully explored
+    PartiallyExplored,  // Left child explored, right child to be explored
+    UnExplored,         // Not yet explored
 }
 
 // Enumeration to represent the maze
@@ -16,7 +16,7 @@ enum Maze {
         label: String,
         left: Arc<Maze>,
         right: Arc<Maze>,
-        status: Mutex<Exploration>, // Interior mutability with Mutex
+        status: Mutex<Exploration>, // Thread-safe interior mutability
     },
     Leaf {
         label: String,
@@ -41,25 +41,27 @@ impl Maze {
         })
     }
 
-    // Concurrent exploration function
-    fn explore(&self, work: &Arc<Mutex<Vec<Arc<Maze>>>>, trace: &mut Vec<String>) {
+    // Exploration function
+    fn explore(&self, work: Arc<Mutex<Vec<Arc<Maze>>>>, trace: &mut Vec<String>) {
         match self {
             Maze::Branch { label, left, right, status } => {
                 let mut current_status = status.lock().unwrap();
                 match *current_status {
                     Exploration::UnExplored => {
+                        // Mark as partially explored and push children to the work stack
                         *current_status = Exploration::PartiallyExplored;
                         trace.push(label.clone());
-                        work.lock().unwrap().push(Arc::clone(left));
-                        work.lock().unwrap().push(Arc::clone(right));
+                        let mut w = work.lock().unwrap();
+                        w.push(Arc::clone(right)); // Push right child first
+                        w.push(Arc::clone(left));  // Push left child second
                     }
                     Exploration::PartiallyExplored => {
+                        // Mark as fully explored
                         *current_status = Exploration::Explored;
                         trace.push(label.clone());
-                        work.lock().unwrap().push(Arc::clone(right));
                     }
                     Exploration::Explored => {
-                        trace.push(label.clone());
+                        // Do nothing, already explored
                     }
                 }
             }
@@ -72,11 +74,13 @@ impl Maze {
 
 // Function to construct the maze
 fn maze() -> Arc<Maze> {
+    // Create the leaves
     let leaf2 = Maze::leaf("2");
     let leaf4 = Maze::leaf("4");
     let leaf5 = Maze::leaf("5");
     let leaf8 = Maze::leaf("8");
 
+    // Create the branches
     let branch3 = Maze::branch("3", Arc::clone(&leaf4), Arc::clone(&leaf5));
     let branch1 = Maze::branch("1", Arc::clone(&leaf2), Arc::clone(&branch3));
     let branch7 = Maze::branch("7", Arc::clone(&leaf5), Arc::clone(&leaf8));
@@ -87,38 +91,40 @@ fn maze() -> Arc<Maze> {
 }
 
 fn main() {
-    // Build the maze
     let maze = maze();
 
-    // Shared work stack and trace
+    // Shared work stack protected by a Mutex
     let work = Arc::new(Mutex::new(vec![Arc::clone(&maze)]));
-    let trace = Arc::new(Mutex::new(Vec::new()));
 
-    // Number of threads
+    // Shared trace output vector (per thread)
     let num_threads = 4;
     let mut handles = vec![];
 
     for i in 0..num_threads {
-        let work_clone = Arc::clone(&work);
-        let trace_clone = Arc::clone(&trace);
+        let work = Arc::clone(&work);
+        let thread_handle = thread::spawn(move || {
+            let mut local_trace = Vec::new();
 
-        // Spawn a thread
-        let handle = thread::spawn(move || {
-            while let Some(node) = work_clone.lock().unwrap().pop() {
-                node.explore(&work_clone, &mut trace_clone.lock().unwrap());
-                println!("Thread {} processed a node", i);
-                thread::yield_now(); // Allow other threads to run
+            while let Some(node) = {
+                let mut w = work.lock().unwrap();
+                if !w.is_empty() {
+                    Some(w.pop().unwrap())
+                } else {
+                    None
+                }
+            } {
+                node.explore(Arc::clone(&work), &mut local_trace);
+                thread::sleep(std::time::Duration::from_millis(100));
+                // thread::yield_now();
             }
+            println!("Worker {} explored nodes: {:?}", i, local_trace);
         });
-        handles.push(handle);
+        handles.push(thread_handle);
     }
 
-    // Wait for all threads to finish
     for handle in handles {
         handle.join().unwrap();
     }
 
-    // Print the final trace
-    let trace_result = trace.lock().unwrap();
-    println!("Final Trace: {:?}", *trace_result);
+    println!("Exploration complete.");
 }
